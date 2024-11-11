f4e70b3c92fe14fc85d7a1239209c64b165926c3
Tapas Mandal <alloymobileinc@gmail.com>
2024-11-11 14:51:55 -0500

    Created a clean project

--- src/app/library/cell/icon/icon.js
+++
@@ -1,8 +0,0 @@
-export default function AlloyIcon(props)
-{
-    return (
-        <span className={props.icon.className}>
-            <i className={props.icon.iconName}></i>
-        </span>
-    )
-}
